1056d77e0d9720b60d167844ab5e0fa9e699629d
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2019-12-16 21:49:57 +0200

    Add files via upload
    second part of assighment
    similar function needs improvement

---
+++ txtfind.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define WORD 30
+#define LINE 256
+
+int getLine(char s[]) {
+    int len = 0;
+//char ch;
+    int size = strlen(s);
+    while (len < size) {
+        scanf("%c", &s[len]);
+        if (s[len] == '\n')break;
+        len++;
+    }
+    return len;
+}
+
+int getword(char w[]) {
+    int len = 0;
+    int size = strlen(w);
+    while (size < len) {
+        scanf("%c", &w[len]);
+        if (w[len] == '\n')break;
+        if (w[len] == '\t')break;
+        len++;
+    }
+    return len;
+
+}
+
+int substring(char *str1, char *str2) {
+
+    int wordlen = 0;
+    int j = 0;
+    int full = strlen(str2);
+    int len = strlen(str1);
+    for (int i = 0; i < len; i++) {
+        if ((str2 + j) == (str1 + i)) {
+            wordlen++;
+            if (wordlen == full)return 1;
+            j++;
+        }
+    }
+    return 0;
+}
+
+int similar(char *s,char *t,int n){
+
+int flag=0;
+for(int j=0;j<n;j++){
+   
+	
+if(substring(*s,*t)==1){flag=1;}
+else{flag
+    
+
+
+
+}
+void print_lines(char *str){
+    for (int i = 0; i < LINE ; ++i) {
+        if(str[i]=='\n')break;
+    printf("%c",str[i])
+    }
+    return;
+}
+void print_similar_words(char *str){
+    for (int i = 0; i < WORD ; ++i) {
+        if(str[i]=='\n')break;
+        if(str[i]=='\t')break;
+        printf("%c",str[i])
+    }
+    return;
+
+}
+        int main(){
+    char opt;
+    char s[LINE];
+    char w[WORD];
+    do {
+        printf("Enter your choice: to search word 'a' or 'b'\n");
+        printf("Enter anything else to quit\n");
+        scanf(" %c", &opt);
+        switch (opt) {
+            case 'a':
+                printf("a\n");
+
+                break;
+            case 'b':
+                printf("b\n");
+
+
+                break;
+            default:
+                opt = 'e';
+                printf("Goodbye\n");
+                break;
+        }
+    } while (opt != 'e');
+    return 0;
+}
+
